3e4cb0cdbda482d2ab73e7be23a3ea80f120f46d
Henry AT <yiperu@gmail.com>
2017-07-22 03:08:08 -0400

    ⚒  Configure concret class

---
+++ Example-Abstract-Factory/001/src/BotonAzul.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yiperu on 7/22/17.
+ */
+public class BotonAzul extends Boton {
+    public BotonAzul(){
+        this.configurar();
+    }
+
+    @Override
+    public void configurar() {
+        System.out.println("Configurando el boton con borde azul");
+    }
+}

301bbfbb3234829373f59aff36609db029564dd0
Henry AT <yiperu@gmail.com>
2017-07-22 03:27:27 -0400

    ⚒  Implement concrete class

---
+++ Example-Abstract-Factory/001/src/ListaAzul.java
@@ -0,0 +1,14 @@
+/**
+ * Created by yiperu on 7/22/17.
+ */
+public class ListaAzul extends Lista {
+
+    public ListaAzul(){
+        this.configurar();
+    }
+
+    @Override
+    public void configurar() {
+        System.out.println("Configurando la LISTA con borde azul");
+    }
+}
---
+++ Example-Abstract-Factory/001/src/ListaRoja.java
@@ -0,0 +1,14 @@
+/**
+ * Created by yiperu on 7/22/17.
+ */
+public class ListaRoja extends Lista {
+
+    public ListaRoja(){
+        this.configurar();
+    }
+
+    @Override
+    public void configurar() {
+        System.out.println("Configurando la Lista con borde Roja");
+    }
+}

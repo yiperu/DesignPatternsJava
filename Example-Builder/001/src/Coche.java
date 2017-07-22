9f7ada5dbad7f7f2f73d0d5d6f3c5c578d1b008e
Henry AT <yiperu@gmail.com>
2017-07-22 04:08:17 -0400

    ✅  Implement coche class

--- Example-Builder/001/src/Coche.java
+++ Example-Builder/001/src/Coche.java
@@ -2,5 +2,45 @@
  * Created by yiperu on 7/22/17.
  */
 public class Coche {
-    
+
+    private String motor = "";
+    private String carroceria = "";
+    private Boolean elevaLunaElec = false;
+    private Boolean airAcond = false;
+
+    public Coche(){
+
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getCarroceria() {
+        return carroceria;
+    }
+
+    public void setCarroceria(String carroceria) {
+        this.carroceria = carroceria;
+    }
+
+    public Boolean getElevaLunaElec() {
+        return elevaLunaElec;
+    }
+
+    public void setElevaLunaElec(Boolean elevaLunaElec) {
+        this.elevaLunaElec = elevaLunaElec;
+    }
+
+    public Boolean getAirAcond() {
+        return airAcond;
+    }
+
+    public void setAirAcond(Boolean airAcond) {
+        this.airAcond = airAcond;
+    }
 }

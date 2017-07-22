bdecb64aebccc6afba4e33b097632fb5a2babf73
Henry AT <yiperu@gmail.com>
2017-07-22 03:05:44 -0400

    ⚒ implements Producto abstract, in this case Boton class

--- Example-Abstract-Factory/001/src/Boton.java
+++ Example-Abstract-Factory/001/src/Boton.java
@@ -4,5 +4,19 @@
 public abstract class Boton {
     private String texto;
     // - - - - - - -
-    
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public void dibujar(){
+        System.out.println("Dibujando el Boton . . . . .");
+    }
+    // = = = = =
+    // Metodo a implementar por las clases que hereden
+    public abstract void configurar();
 }

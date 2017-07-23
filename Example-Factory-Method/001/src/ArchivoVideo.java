3500fa36822cf70011c49d17fb18815a427ccaed
Henry AT <yiperu@gmail.com>
2017-07-22 22:15:18 -0400

    📄  Make ArchivoVideo Class

---
+++ Example-Factory-Method/001/src/ArchivoVideo.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yiperu on 7/22/17.
+ */
+public class ArchivoVideo implements IArchivo {
+
+    public ArchivoVideo() {
+    }
+
+    @Override
+    public void reproducir() {
+        System.out.println("Reproduciendo archivo de video......");
+    }
+}

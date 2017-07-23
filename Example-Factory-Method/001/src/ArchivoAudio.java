d11d79b67f2e0b7c7a7a60e26d112aa8a1b30372
Henry AT <yiperu@gmail.com>
2017-07-22 22:14:10 -0400

    📄  Make ArchivoAudio Class

---
+++ Example-Factory-Method/001/src/ArchivoAudio.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yiperu on 7/22/17.
+ */
+public class ArchivoAudio implements IArchivo {
+
+    public ArchivoAudio() {
+    }
+
+    @Override
+    public void reproducir() {
+        System.out.println("Reproduciendo archivo de Audio");
+    }
+}

80a3b02f4e9dda6016922d186e6c4eda3b583c7f
Henry AT <yiperu@gmail.com>
2017-08-08 16:44:39 -0400

    ⚒ Make pricipal class that call singleton

---
+++ Example-Singleton/001/src/Principal.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yiperu on 8/8/17.
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        for (int num =1;num <= 5; num++){
+            Coche.getInstancia();
+        }
+    }
+}
